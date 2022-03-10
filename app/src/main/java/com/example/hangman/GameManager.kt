169092bc5fd1af60987c75c2f95cd07d709d45b8
NatPuff <cesarp102104@gmail.com>
2022-03-10 10:48:08 -0600

    Initial commit

---
+++ app/src/main/java/com/example/hangman/GameManager.kt
@@ -0,0 +1,91 @@
+package com.example.hangman
+
+import kotlin.random.Random
+
+class GameManager {
+
+    private var lettersUsed: String = ""
+    private lateinit var underscoreWord: String
+    private lateinit var wordToGuess: String
+    private val maxTries = 7
+    private var currentTries = 0
+    private var drawable: Int = R.drawable.game0
+
+    fun generateUnderScores(word: String){
+    val sb = StringBuilder()
+    word.forEach { char ->
+        if(char == '/'){
+            sb.append('/')
+        } else{
+            sb.append("_")
+        }
+
+        }
+        underscoreWord = sb.toString()
+    }
+
+    private fun getHangmanDrawable(): Int{
+        return when (currentTries){
+            0-> R.drawable.game0
+            1-> R.drawable.game1
+            2-> R.drawable.game2
+            3-> R.drawable.game3
+            4-> R.drawable.game4
+            5-> R.drawable.game5
+            6-> R.drawable.game6
+            7-> R.drawable.game7
+            else -> R.drawable.game7
+        }
+        }
+
+    private fun getGameState(): GameState{
+        if(underscoreWord.equals(wordToGuess, true))
+            return GameState.Won(wordToGuess)
+
+        if(currentTries == maxTries){
+            return GameState.Lost(wordToGuess)
+        }
+
+        drawable = getHangmanDrawable()
+        return GameState.Running(lettersUsed, underscoreWord, drawable)
+
+    }
+
+    fun startNewGame(): GameState{
+        lettersUsed = ""
+        currentTries = 0
+        drawable = R.drawable.game7
+        val randomIndex = Random.nextInt(0, GameWords.words.size)
+        wordToGuess = GameWords.words[randomIndex]
+        generateUnderScores(wordToGuess)
+        return getGameState()
+    }
+
+    fun play(letter: Char): GameState {
+        if(lettersUsed.contains(letter)){
+            return GameState.Running(lettersUsed, underscoreWord, drawable)
+
+        }
+
+        lettersUsed += letter
+        val indexes = mutableListOf<Int>()
+
+        wordToGuess.forEachIndexed{ index, char ->
+            if(char.equals(letter, true)){
+                indexes.add(index)
+            }
+        }
+        var finalUnderScoreWord = "" + underscoreWord
+        indexes.forEach{ index ->
+            val sb = StringBuilder(finalUnderScoreWord).also {
+                it.setCharAt(index, letter)
+            }
+            finalUnderScoreWord = sb.toString()
+        }
+        if(indexes.isEmpty()){
+            currentTries++
+        }
+        underscoreWord = finalUnderScoreWord
+        return getGameState()
+    }
+}
---
+++ app/src/main/java/com/example/hangman/GameWords.kt
@@ -0,0 +1,7 @@
+package com.example.hangman
+
+object GameWords {
+    val words = listOf(
+        "Jigglypuff", "Stinky", "Sus", "Sussy Baka", "Hog Rider", "I'm/not/a/child", "Free/Speech", "Poopooegg", "Imposter", "Jigglysus", "Jigglybuff", "Holypuff", "I/am/your/child", "Sauce", "You/are/the/father", "chef", "Fossil", "Princess/Diana", "Gordon/Ramsey"
+    )
+}
